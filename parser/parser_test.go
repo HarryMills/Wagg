685bc709322a3a89724753545eab358a9d61b31f
HarryMills <harry.e.mills@gmail.com>
2020-05-20 17:05:20 +0100

    Parsing bork statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -81,3 +81,35 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 	t.FailNow()
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	bork 5;
+	bork 10;
+	bork 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. Got =%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. Got=%T",
+				stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "bork" {
+			t.Errorf("returnStmt.TokenLiteral not 'bork', got=%q",
+				returnStmt.TokenLiteral())
+		}
+	}
+}
