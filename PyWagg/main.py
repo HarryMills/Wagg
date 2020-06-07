34949e2f4ec3d1c4675904db177d9755fd3d1d73
HarryMills <harry.e.mills@gmail.com>
2020-06-07 15:39:06 +0100

    Sorting out Unittest for Python

--- PyWagg/main.py
+++ PyWagg/main.py
@@ -1,11 +1,9 @@
 import getpass
 from PyWagg import repl
-from PyWagg import tokens
 
 
 def main():
     user = getpass.getuser()
-    print(tokens.keywords.get("floof"))
     print("Hello %s! This is the Wagg programming language!" % user)
     print("Feel free to type in commands:")
     repl.start()
